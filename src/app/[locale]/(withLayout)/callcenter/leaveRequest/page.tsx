4234daf8b2b48eda37ba596752d48f57f4275728
Muhammad <muxammad_205@mail.ru>
2024-08-06 09:52:08 +0500

    fix(eslint): Errors eslint belongs control page

--- src/app/[locale]/(withLayout)/callcenter/leaveRequest/page.tsx
+++ src/app/[locale]/(withLayout)/callcenter/leaveRequest/page.tsx
@@ -1,7 +1,6 @@
 import { LeaverequestAsync } from "@/@core/pages/Callcenter/Leaverequest";
 import { FC } from "react";
 
-const Leaverequest: FC = () => {
-  return <LeaverequestAsync />;
-};
+const Leaverequest: FC = () => <LeaverequestAsync />;
+
 export default Leaverequest;
