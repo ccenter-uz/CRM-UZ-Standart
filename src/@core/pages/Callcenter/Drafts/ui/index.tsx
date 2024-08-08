076deb2b9721eabc0fc3093d71d22fd98459a612
Muhammad <muxammad_205@mail.ru>
2024-08-08 16:17:47 +0500

    fix(search): Fixed search filter

--- src/@core/pages/Callcenter/Drafts/ui/index.tsx
+++ src/@core/pages/Callcenter/Drafts/ui/index.tsx
@@ -49,9 +49,10 @@ export const Drafts: FC = () => {
     const query = {
       page: current,
       pageSize: pageSize,
-      phone: params.get("phone")
-        ? `+${String(params.get("phone")).trim()}`
-        : "null",
+      phone:
+        params.has("phone") && params.get("phone") !== "null"
+          ? `+${String(params.get("phone")).trim()}`
+          : "null",
       applicant_birthday: params.get("applicant_birthday") || "null",
       applicant: params.get("applicant") || "",
       operators: params.get("operators") || "null",
